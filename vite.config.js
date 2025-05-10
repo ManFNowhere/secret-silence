ea188d904279f09530017235ea67d7d937e2f99f
Man From Nowhere <86987720+ManFNowhere@users.noreply.github.com>
2025-05-10 09:59:28 +0200

    modif vite

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,14 @@ export default defineConfig({
         tailwindcss(),
     ],
     server: {
-        host: '127.0.0.1', 
+        // Di production, sebaiknya tidak menggunakan host 127.0.0.1
+        // Jika perlu akses dari berbagai perangkat, bisa menggunakan '0.0.0.0'
+        // host: '0.0.0.0', // Atau kamu bisa menghapus ini jika tidak perlu akses khusus
+        port: 5173, // Sesuaikan port sesuai kebutuhan
+        https: true, // Sesuaikan apakah perlu HTTPS di server produksi
     },
+    build: {
+        outDir: 'public/build',  // Pastikan output build ditempatkan di folder yang sesuai
+        manifest: true,           // Agar manifest dibuat untuk produksi
+    }
 });
