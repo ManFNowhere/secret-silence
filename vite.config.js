fc92433875039c105accce423f86eed43b0ad43f
Man From Nowhere <86987720+ManFNowhere@users.noreply.github.com>
2025-05-10 09:48:42 +0200

    go

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,10 @@ export default defineConfig({
         tailwindcss(),
     ],
     server: {
-        host: '127.0.0.1', 
+        hmr: {
+            host: 'localhost',
+        },
+        host: true,
+        origin: 'https://secret-silence-main-zwyhwc.laravel.cloud',
     },
 });
